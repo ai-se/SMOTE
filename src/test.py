2d382bfb59c9e8270a487c716a1b44d37f839bce
WeiFoo <wfu@ncsu.edu>
2015-11-20 16:36:59 -0500

    add MPI test

--- src/test.py
+++ src/test.py
@@ -1,7 +1,10 @@
 from __future__ import print_function
 from mpi4py import MPI
 
+x = range(9)
 # __author__ = 'WeiFu'
+rank = MPI.COMM_WORLD.Get_rank()
+out = x[rank+1]
 print("hello world")
-print("my rank is :%d"%MPI.COMM_WORLD.Get_rank())
+print("my rank is :%s"%str(MPI.COMM_WORLD.Get_rank()), "my number:"+str(out))
 
