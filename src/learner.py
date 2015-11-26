2b60cbc42b2f10f5fe5a5ae898ed9fd83dadd2ee
WeiFoo <wfu@ncsu.edu>
2015-11-25 19:30:14 -0500

    refactor learner.py to accommodate tuning

--- src/learner.py
+++ src/learner.py
@@ -12,182 +12,164 @@ from newabcd import *
 from collections import Counter
 import numpy as np
 import pdb
- # __author__ = 'WeiFu'
 
 
+# __author__ = 'WeiFu'
+
+class Learners(object):
+  def __init__(self, clf, train_X, train_Y, predict_X, predict_Y, F, goal="F"):
+    self.train_X = train_X
+    self.train_Y = train_Y
+    self.predict_X = predict_X
+    self.predict_Y = predict_Y
+    self.F = F
+    self.goal = goal
+    self.param_distribution = self.get_param()
+    self.learner = clf
+
+  def learn(self, **kwargs):
+    self.learner.set_params(**kwargs)
+    clf = self.learner.fit(self.train_X, self.train_Y)
+    predictresult = clf.predict(self.predict_X)
+    scores = self._Abcd(predictresult, self.predict_Y, self.F)
+    return scores
+
+  def _Abcd(predicted, actual, F):
+    """
+    get performance scores. not test  yet!!! 1120
+    """
+
+    def calculate(scores):
+      for i, v in enumerate(scores):
+        F[uni_actual[i]] = F.get(uni_actual[i], []) + [v]
+      freq_actual = [count_actual[one] / len(actual) for one in uni_actual]
+      F["mean"] = F.get("mean", []) + [np.mean(scores)]
+      F["mean_weighted"] = (F.get("mean_weighted", []) + [
+        np.sum(np.array(scores) * np.array(freq_actual))])
+      return F
+
+    # pdb.set_trace()
+    abcd = ABCD(actual, predicted)
+    uni_actual = list(set(actual))
+    count_actual = Counter(actual)
+    score_each_klass = [k.stats()[-2] for k in abcd()]  # -2 is F measure
+    return calculate(score_each_klass)
+
+  def get_param(self):
+    raise NotImplementedError("You should implement get_param function")
+
+class CartClassifier(Learners):
+  def __init__(self, train_x, train_y, predict_x, predict_y, F):
+    clf = DecisionTreeClassifier()
+    super(CartClassifier, self).__init__(clf,train_x, train_y, predict_x, predict_y, F)
+
+  def get_param(self):
+    tunelst = {
+                "max_features": [0.01, 1],
+                "max_depth": [1, 50],
+                "min_samples_split": [2, 20],
+                "min_samples_leaf": [1, 20]
+              }
+    return tunelst
+
+class RfClassifier(Learners):
+  def __init__(self, train_x, train_y, predict_x, predict_y, F):
+    clf =  RandomForestClassifier()
+    super(RfClassifier, self).__init__(clf, train_x, train_y, predict_x, predict_y, F)
+
+  def get_param(self):
+    tunelst = {
+                 "min_samples_split":[1,20],
+                 "min_samples_leaf ":[2,20],
+                 "max_leaf_nodes":[10,50],
+                 "n_estimators":[50,150],
+                 "max_features":[0.01,1]
+              }
+    return tunelst
+
+class Naive_bayes(Learners):
+  def __init__(self,train_x, train_y, predict_x, predict_y, F):
+    clf = Naive_bayes()
+    super(Naive_bayes, self).__init__(clf,train_x, train_y, predict_x, predict_y, F)
+
+  def get_param(self):
+    tunelst = {}
+    return tunelst
+
+class Linear_SVM(Learners):
+  def __init__(self,train_x, train_y, predict_x, predict_y, F):
+    super(Linear_SVC, self).__init__(train_x, train_y, predict_x, predict_y, F)
+
+  def get_param(self):
+    tunelst = {}
+    return tunelst
+
+
+
+# def linear_SVM(train_x, train_y, predict_x, predict_y, F, **kwargs):
+#   clf = LinearSVC(dual=False, **kwargs)
+#   return learn(clf, train_x, train_y, predict_x, predict_y, F)
 #
-# class Learner(object):
-#   def __init__(i, train, tune, test):
-#     i.train = train
-#     i.tune = tune
-#     i.test = test
-#     i.predict = None
 #
-#   def untuned(i):
-#     i.default()
-#     i.predict = i.test
-#     score = i.call()
-#     return score
-#
-#   def tuned(i):
-#     i.predict = i.tune
-#     i.optimizer()
-#     The.option.tuning = False
-#     The.data.predict = i.test
-#     score = i.call()
-#     return score
-#
-#   def call(i):
-#     raise NotImplementedError
-#
-#   def optimizer(i):
-#     raise NotImplementedError
-#
-#   def default(i):
-#     raise NotImplementedError
-#
-#   def learn(i, clf, train_X, train_Y, predict):
-#
-#     clf = clf.fit(train_X, train_Y)
-#     array = clf.predict(test_X)
-#     predictresult = [i for i in array]
-#     scores = _Abcd(predictresult, test_Y)
-#     return scores
-#
-#
-#
-#
-# class CART(Learner):
-#   def __init__(i, train, tune, predict):
-#     super(CART, i).__init__(train, tune, predict)
-#     i.tunelst = ["The.cart.max_features",
-#                  "The.cart.max_depth",
-#                  "The.cart.min_samples_split",
-#                  "The.cart.min_samples_leaf",
-#                  "The.option.threshold"]
-#     i.tune_min = [0.01, 1, 2, 1, 0.01]
-#     i.tune_max = [1, 50, 20, 20, 1]
-#
-#   def default(i):
-#     The.cart.max_features = None
-#     The.cart.max_depth = None
-#     The.cart.min_samples_split = 2
-#     The.cart.min_samples_leaf = 1
-#     The.option.threshold = 0.5
-#
-#   def call(i):
-#     return cart(i.train, i.predict)
-#
-#   def optimizer(i):
-#     tuner = CartDE(i)
-#     tuner.DE()
-#
-#
-# class CART_clf(CART):
-#   def __init__(i, train, tune, predict):
-#     super(CART_clf, i).__init__(train, tune, predict)
-#
-#   def call(i):
-#     return cartClassifier(i.train, i.predict)
-#
-#
-# class RF(Learner):
-#   def __init__(i, train, tune, predict):
-#     super(RF, i).__init__(train, tune, predict)
-#     i.tunelst = ["The.rf.min_samples_split",
-#                  "The.rf.min_samples_leaf ",
-#                  "The.rf.max_leaf_nodes",
-#                  "The.rf.n_estimators",
-#                  "The.rf.max_features",
-#                  "The.option.threshold"]
+# def naive_bayes(self.train_x, train_y, predict_x, predict_y, F, **kwargs):
+#   super(naive_bayes, self).__init__(train_x, train_y, predict_x,
+#                                          predict_y, F)
+#   clf = MultinomialNB(**kwargs)
+#   return learn(clf, train_x, train_y, predict_x, predict_y, F)
+
 #     i.tune_min = [1, 2, 10, 50, 0.01, 0.01]
 #     i.tune_max = [20, 20, 50, 150, 1, 1]
-#     i.default_value = [2,1,None, 100,"auto",0.5]
+
+# def _Abcd(predicted, actual, F):
+#   """
+#   get performance scores. not test  yet!!! 1120
+#   """
 #
-#   def default(i):
-#     # for key,val in zip(i.tunelst,i.default_value):
-#     #   setattr(key[],key[4:],val)
-#     # pdb.set_trace()
-#     The.option.threshold = 0.5
-#     The.rf.max_features = "auto"
-#     The.rf.min_samples_split = 2
-#     The.rf.min_samples_leaf = 1
-#     The.rf.max_leaf_nodes = None
-#     The.rf.n_estimators = 100
+#   def calculate(scores):
+#     for i, v in enumerate(scores):
+#       F[uni_actual[i]] = F.get(uni_actual[i], []) + [v]
+#     freq_actual = [count_actual[one] / len(actual) for one in uni_actual]
+#     F["mean"] = F.get("mean", []) + [np.mean(scores)]
+#     F["mean_weighted"] = (F.get("mean_weighted", []) + [
+#       np.sum(np.array(scores) * np.array(freq_actual))])
+#     return F
 #
-#   def call(i): return rf(i.train, i.predict)
-#
-# class RF_clf(RF):
-#   def __init__(i,train,tune,predict):
-#     super(RF_clf, i).__init__(train,tune,predict)
-#
-#   def call(i): return rfClassifier(i.train, i.predict)
+#   # pdb.set_trace()
+#   abcd = ABCD(actual, predicted)
+#   uni_actual = list(set(actual))
+#   count_actual = Counter(actual)
+#   score_each_klass = [k.stats()[-2] for k in abcd()]  # -2 is F measure
+#   return calculate(score_each_klass)
 #
 #
-#   def optimizer(i):
-#     tuner = RfDE(i)
-#     tuner.DE()
+# def learn(clf, train_X, train_Y, predict_X, predict_Y, F):
+#   clf = clf.fit(train_X, train_Y)
+#   predictresult = clf.predict(predict_X)
+#   scores = _Abcd(predictresult, predict_Y, F)
+#   return scores
 
 
-def _Abcd(predicted, actual, F):
-  """
-  get performance scores. not test  yet!!! 1120
-  """
-  def calculate(scores):
-    for i, v in enumerate(scores):
-      F[uni_actual[i]] = F.get(uni_actual[i],[]) +[v]
-    freq_actual = [count_actual[one]/len(actual) for one in uni_actual]
-    F["mean"] = F.get("mean",[]) + [np.mean(scores)]
-    F["mean_weighted"] = (F.get("mean_weighted",[]) +
-                         [np.sum(np.array(scores)*np.array(freq_actual))])
-    return F
-  # pdb.set_trace()
-  abcd = ABCD(actual, predicted)
-  uni_actual = list(set(actual))
-  count_actual = Counter(actual)
-  score_each_klass = [ k.stats()[-2]for k in abcd()]  # -2 is F measure
-  return calculate(score_each_klass)
+# def cartClassifier(train_x, train_y, predict_x, predict_y, F, **kwargs):
+#   clf = DecisionTreeClassifier(**kwargs)
+#   return learn(clf, train_x, train_y, predict_x, predict_y, F)
+#
+#
+# def rfClassifier(train, train_x, train_y, predict_x, predict_y, F, **kwargs):
+#   clf = RandomForestClassifier(**kwargs)
+#   # pdb.set_trace()
+#   return learn(clf, train_x, train_y, predict_x, predict_y, F)
 
 
-def learn(clf, train_X, train_Y, predict_X, predict_Y, F):
-  clf = clf.fit(train_X, train_Y)
-  predictresult = clf.predict(predict_X)
-  scores = _Abcd(predictresult, predict_Y,F)
-  return scores
-
-
-def cartClassifier(train_x, train_y, predict_x, predict_y, F):
-  clf = DecisionTreeClassifier(
-    max_features=The.cart.max_features,
-    max_depth=The.cart.max_depth,
-    min_samples_split=The.cart.min_samples_split,
-    min_samples_leaf=The.cart.min_samples_leaf,
-    random_state=1)
-  return learn(clf, train_x, train_y, predict_x, predict_y, F)
-
-
-def rfClassifier(train, train_x, train_y, predict_x, predict_y, F):
-  clf = RandomForestClassifier(
-    n_estimators=The.rf.n_estimators,
-    max_features=The.rf.max_features,
-    min_samples_split=The.rf.min_samples_split,
-    min_samples_leaf=The.rf.min_samples_leaf,
-    max_leaf_nodes=The.rf.max_leaf_nodes,
-    random_state=1)
-  # pdb.set_trace()
-  return learn(clf, train_x, train_y, predict_x, predict_y, F)
-
-
-def naive_bayes(train_x, train_y, predict_x, predict_y, F):
-  clf = MultinomialNB()
-  return learn(clf,train_x, train_y, predict_x, predict_y, F)
-
-
-def linear_SVM(train_x, train_y, predict_x, predict_y, F):
-  clf = LinearSVC(dual=False)
-  return learn(clf,train_x, train_y, predict_x, predict_y, F)
-
-
-def logistic():
-  clf = linear_model.LogisticRegression()
-  return learn(clf)
+# def naive_bayes(train_x, train_y, predict_x, predict_y, F, **kwargs):
+#   clf = MultinomialNB(**kwargs)
+#   return learn(clf, train_x, train_y, predict_x, predict_y, F)
+#
+#
+# def linear_SVM(train_x, train_y, predict_x, predict_y, F, **kwargs):
+#   clf = LinearSVC(dual=False, **kwargs)
+#   return learn(clf, train_x, train_y, predict_x, predict_y, F)
+#
+#
+# def logistic():
+#   clf = linear_model.LogisticRegression()
+#   return learn(clf)
