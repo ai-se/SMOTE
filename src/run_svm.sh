bfc996eed7714391acc0427dcd035eb90426f6dd
WeiFoo <wfu@ncsu.edu>
2016-02-01 22:32:56 -0500

    updage

--- src/run_svm.sh
+++ src/run_svm.sh
@@ -9,8 +9,6 @@ rm err_svm/*
 #  bsub -W 1000 -n 4 -o ./out/$VAR.out.%J -e ./err/$VAR.err.%J mpiexec -n 4 /share3/wfu/miniconda/bin/python2.7 textMining.py run /share3/wfu/Datasets/StackExchange/$VAR.txt
 #end
 
-#rm out_test/*
-#rm err_test/*
 
 
 ##### this is only for testing on HPC
